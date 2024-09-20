c7d42730f1e21f6a3c67f9ca52ef751e337e31d1
Graypbj <graypbj@gmail.com>
2024-09-19 19:50:25 -0600

    Updated game to be look better in the CLI

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -9,11 +9,13 @@ game_running = True
 
 # Print the game board
 def print_board(board):
+    print("\n\n")
     print("{} | {} | {}".format(board[0], board[1], board[2]))
     print("----------")
     print("{} | {} | {}".format(board[3], board[4], board[5]))
     print("----------")
     print("{} | {} | {}".format(board[6], board[7], board[8]))
+
 
 
 # Take player input
